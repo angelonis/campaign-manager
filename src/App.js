36da4e2b22ccb96178e488b730eaf81cc764ee68
Scott Angeloni <58601712+angelonis@users.noreply.github.com>
2025-06-19 10:46:03 -0400

    Created initial site (#3)
    * main test 3
    
    * Initial site build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import NPCPage from "./pages/NPCPage";
+import QuestPage from "./pages/QuestPage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <nav style={{ padding: "1rem", backgroundColor: "#222", color: "#fff" }}>
+                <Link to="/" style={{ marginRight: 10, color: "#fff" }}>Home</Link>
+                <Link to="/npcs" style={{ marginRight: 10, color: "#fff" }}>NPCs</Link>
+                <Link to="/quests" style={{ color: "#fff" }}>Quests</Link>
+            </nav>
+
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/npcs" element={<NPCPage />} />
+                <Route path="/quests" element={<QuestPage />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/pages/NPCPage.js
@@ -0,0 +1,4 @@
+function NPCPage() {
+    return <h2>NPC Manager (coming soon)</h2>;
+}
+export default NPCPage;
